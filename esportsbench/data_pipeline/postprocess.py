7759fb05e8a22aa07aea46cb079d960ab0154442
cthorrez <claytonthorrez@gmail.com>
2024-05-25 22:49:19 -0400

    train test split

--- esportsbench/data_pipeline/postprocess.py
+++ esportsbench/data_pipeline/postprocess.py
@@ -8,7 +8,7 @@ from esportsbench.constants import GAME_NAME_MAP
 pl.Config.set_tbl_rows(100)
 
 
-def main(end_date, min_rows_year):
+def main(train_end_date, test_end_date, min_rows_year):
     data_dir = pathlib.Path(__file__).resolve().parents[2] / 'data'
     input_file_paths = data_dir.glob('full_data/*')
     for input_file_path in input_file_paths:
@@ -18,8 +18,6 @@ def main(end_date, min_rows_year):
         print(f'num total matches: {len(df)}')
         df = df.with_columns(pl.col('date').str.to_datetime().alias('date'))
         df = df.with_columns(pl.col('date').dt.year().alias('year'))
-        future_df = df.filter(pl.col('date') >= datetime.now())
-        print('num future matches:', len(future_df))
         year_counts = df.group_by('year').count().sort('year')
         first_valid_year = year_counts.filter(
             pl.col('count') >= min_rows_year
@@ -29,11 +27,26 @@ def main(end_date, min_rows_year):
         df = df.with_columns(
             pl.col('date').dt.date().cast(pl.Utf8).alias('date')
         )
+        future_df = df.filter(pl.col('date') > datetime.today().strftime("%Y-%m-%d"))
+        print('num future matches:', len(future_df))
+
         df = df.filter(
             (pl.col('year') >= first_valid_year)
-            & (pl.col('date') <= end_date)
+            & (pl.col('date') <= test_end_date)
         )
         print(f'filtered row count: {len(df)}')
+
+        train_df = df.filter(
+            (pl.col('date') <= train_end_date)
+        )
+        test_df = df.filter(
+            (pl.col('date') > train_end_date)
+            & (pl.col('date') <= test_end_date)
+        )
+
+        print(f'num train rows: {len(train_df)}')
+        print(f'num test rows: {len(test_df)}')
+
 
         output_file_path = data_dir / 'final_data' / input_file_path.name
         df.write_csv(output_file_path)
@@ -43,7 +56,8 @@ def main(end_date, min_rows_year):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--end_date', type=str, default='2024-03-31', help='inclusive end date for final data')
+    parser.add_argument('--train_end_date', type=str, default='2023-03-31', help='inclusive end date for test set')
+    parser.add_argument('--test_end_date', type=str, default='2024-03-31', help='inclusive end date for test set')
     parser.add_argument('--min_rows_year', type=int, default=100, help='minmum number of rows in a year to begin including data')
     args = vars(parser.parse_args())
     main(**args)
