4969bc63015fda0dfc0c90a0056621427c2a4435
cthorrez <claytonthorrez@gmail.com>
2024-07-04 22:15:49 -0700

    start trying to refactor experiment setup

--- esportsbench/eval/rating_experiment.py
+++ esportsbench/eval/rating_experiment.py
@@ -10,8 +10,29 @@ from omegaconf import DictConfig
 from esportsbench.eval.sweep import sweep
 from esportsbench.constants import GAME_SHORT_NAMES
 
-def main():
-    pass
+# Suppress overflow warnings since many of the combinations swept over are expected to be numerically unstable
+warnings.filterwarnings('ignore', category=RuntimeWarning)
+
+@hydra.main(version_base=None, config_path="conf", config_name="config")
+def main(config: DictConfig):
+    if config.games == 'all':
+        games = GAME_SHORT_NAMES
+    else:
+        games = config.games
+
+
+    sweep(
+        games=games,
+        rating_systems=config.rating_systems,
+        data_dir=config.data_dir,
+        granularity='broad',
+        sweep_config=config.sweep_config,
+        rating_period=config.rating_period,
+        train_end_date=config.train_end_date,
+        test_end_date=config.test_end_date,
+        num_samples=config.num_samples,
+        num_processes=config.num_processes,
+    )
 
 if __name__ == '__main__':
     main()
