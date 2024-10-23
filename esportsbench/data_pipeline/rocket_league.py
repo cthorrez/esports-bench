7da42a0fe716cab2f0dd9aad129facb1cc7d19de
cthorrez <claytonthorrez@gmail.com>
2024-10-22 23:41:11 -0700

    account for dynamic extradata schema

--- esportsbench/data_pipeline/rocket_league.py
+++ esportsbench/data_pipeline/rocket_league.py
@@ -1,4 +1,5 @@
 """classes and functions for ingesting and processing rocket league data"""
+import json
 import polars as pl
 from esportsbench.data_pipeline.data_pipeline import LPDBDataPipeline
 from esportsbench.utils import is_null_or_empty, invalid_date_expr, debug_ndjson
@@ -9,6 +10,7 @@ class RocketLeagueDataPipeline(LPDBDataPipeline):
 
     game = 'rocket_league'
     version = 'v3'
+    schema_overrides = {'extradata': json.dumps}
     request_params_groups = {
         'rocket_league.jsonl': {
             'wiki': 'rocketleague',
@@ -25,7 +27,7 @@ class RocketLeagueDataPipeline(LPDBDataPipeline):
     def process_data(self):
         df = pl.scan_ndjson(
             self.raw_data_dir / 'rocket_league.jsonl',
-            infer_schema_length=158638,
+            infer_schema_length=162297,
             ignore_errors=False
         ).collect()
         print(f'initial row count: {df.shape[0]}')
